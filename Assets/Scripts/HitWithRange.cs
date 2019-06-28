bfc538e2704770afcc24d17e1834453a4d8a0409
Thomas Stassin <thomas@panopticgame.com>
2019-06-28 09:59:45 +0200

    exercice 2

---
+++ Assets/Scripts/HitWithRange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitWithRange : MonoBehaviour
+{
+    public float viewRange = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Color color = Color.green;
+
+        if (Physics.Raycast(transform.position, transform.forward, viewRange)){
+            Debug.Log("There something...");
+            color = Color.red;
+        }
+        Debug.DrawRay(transform.position, transform.forward * viewRange, color);
+    }
+}
