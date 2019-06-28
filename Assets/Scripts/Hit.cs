82380649f95a809f3617e49bebbff6310ca3d263
Thomas Stassin <thomas@panopticgame.com>
2019-06-28 09:29:28 +0200

    first commit

---
+++ Assets/Scripts/Hit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.forward * 100, Color.yellow);
+        if (Physics.Raycast(transform.position, transform.forward)){
+            Debug.Log("There something...");
+        }
+    }
+}
