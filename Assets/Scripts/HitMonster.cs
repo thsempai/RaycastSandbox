7e01877551e655f41464458a98548c3a25799f4c
Thomas Stassin <thomas@panopticgame.com>
2019-06-28 10:40:10 +0200

    exercice 3

---
+++ Assets/Scripts/HitMonster.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitMonster : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit)) {
+            Color color = Color.blue;
+            if (hit.transform.tag == "Monstre") {
+                Debug.Log("It's a Monster");
+                color = new Color(1f, 0.5f, 0.5f);
+            }
+
+            Debug.DrawRay(transform.position, transform.forward * 100, color);
+        }
+
+    }
+}
